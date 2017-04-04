90b4151ac27159bb38fae32f2c6ef9e436fcc7ac
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-04-04 02:36:01 +0200

    OrderTest - utworzenie

---
+++ src/test/java/lab3_3/OrderTest.java
@@ -0,0 +1,19 @@
+package lab3_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrderTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
