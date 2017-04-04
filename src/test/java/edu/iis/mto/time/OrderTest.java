632720c6a7bb2234cfb2dfd20c8741a482ad0a69
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-04-04 03:01:18 +0200

    OrderTest - utworzenie i impl testu sprawdzajacego stan po utworzeniu zamowienia

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,11 +1,15 @@
 package edu.iis.mto.time;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import edu.iis.mto.faketime.FakeTimeSource;
+import edu.iis.mto.faketime.RealTimeSource;
+import edu.iis.mto.time.Order.State;
 
 public class OrderTest {
 
@@ -19,6 +23,12 @@ public class OrderTest {
 		Order order = new Order(new FakeTimeSource());
 		order.submit();
 		order.confirm();
+	}
+	
+	@Test
+	public void testNewOrderShouldHaveStateCreated() {
+		Order order = new Order(new RealTimeSource());
+		Assert.assertThat(order.getOrderState(), is(equalTo(State.CREATED)));
 	}
 
 }
